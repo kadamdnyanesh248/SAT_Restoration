7e69b5f318d9e29b8cf3355bf626c1f5f17a7fb4
Dnyaneshwar Kadam <47293435+kadamdnyanesh248@users.noreply.github.com>
2022-08-23 15:35:25 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const plugins = [
+  tailwindcss('./tailwind.config.js'),
+  autoprefixer({
+    // add: true,
+    // grid: true,
+  }),
+];
+
+module.exports = {
+  plugins,
+};
